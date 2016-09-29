d318238721a9b1a0145575224bf600a720d82533
Mauricio Jost <mauricio.jost@gmail.com>
2016-09-29 23:16:09 +0200

    Refactoring

--- src/Zorrino.cpp
+++ src/Zorrino.cpp
@@ -33,9 +33,7 @@
 #include <avr/power.h>
 #include <avr/wdt.h>
 
-
-
-#define DELAY 3000
+#define DELAY 100
 #define BUTTON0 UC_PIN_2
 #define BUTTON1 UC_PIN_3
 #define BUILTIN_LED UC_PIN_13
@@ -49,118 +47,72 @@
 #define LCD_D7_PIN UC_PIN_4
 
 
+volatile bool wdt_was_triggered = true;
+volatile bool button0_was_triggered = false;
+volatile bool button1_was_triggered = false;
 LiquidCrystal lcd(LCD_RS_PIN, LCD_ENABLE_PIN, LCD_D4_PIN, LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
 Servo servo;
 UI ui;
 
-volatile int f_wdt=1;
-
 /*
-void timerIsr() {
-  lcd.setCursor(0, 0); lcd.print("TIMIS"); delay(DELAY);
-  ui.go(digitalRead(BUTTON0), digitalRead(BUTTON1), HIGH);
-  digitalWrite(BUILTIN_LED, !digitalRead(BUILTIN_LED));
+size_t debug(const __FlashStringHelper * msg) {
+    Serial.println(msg);
+    delay(100);
+}
+*/
+
+size_t debug(const char * msg) {
+    Serial.println(msg);
+    delay(100);
 }
 
-*/
 void wakeUpButton0() {
-  Serial.println("Int button 0");
+    if (!button0_was_triggered) {
+        button0_was_triggered = true;
+    } else {
+        debug("button 0 overpressed!!!");
+    }
 }
 
 void wakeUpButton1() {
-  Serial.println("Int button 1");
+    if (!button1_was_triggered) {
+        button1_was_triggered = true;
+    } else {
+        debug("button 1 overpressed!!!");
+    }
 }
-
-/*
-void sleepNow()         // here we put the arduino to sleep
-{
-
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here
-
-    sleep_enable();          // enables the sleep bit in the mcucr register
-                             // so sleep is possible. just a safety pin
-
-    attachInterrupt(digitalPinToInterrupt(BUTTON0), wakeUp, RISING);
-    attachInterrupt(digitalPinToInterrupt(BUTTON1), wakeUp, RISING);
-
-    lcd.setCursor(0, 0); lcd.print("SLEEP"); delay(DELAY);
-    sleep_mode();            // here the device is actually put to sleep!!
-                             // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
-
-    sleep_disable();         // first thing after waking from sleep:
-                             // disable sleep...
-                             // disables interrupt 0 on pin 2 so the
-                             // wakeUpNow code will not be executed
-                             // during normal running time.
-    lcd.setCursor(0, 0); lcd.print("AWAKE"); delay(DELAY);
-    detachInterrupt(digitalPinToInterrupt(BUTTON0));
-    detachInterrupt(digitalPinToInterrupt(BUTTON1));
-
-
-}
-*/
-/*
-void setup() {
-  //Timer1.initialize(100000); // set a timer of length 100000 microseconds (or 0.1 sec - or 10Hz => the led will blink 5 times, 5 cycles of on-and-off, per second)
-  Timer1.attachInterrupt(timerIsr); // attach the service routine here
-
-}
-
-
-void loop() {
-  lcd.setCursor(0, 0); lcd.print("LOOPI"); delay(DELAY);
-  Timer1.initialize(ui.get_timer_period_ms());
-  sleepNow();
-  Timer1.initialize(ui.get_timer_period_ms());
-}
-*/
-
 
 ISR(WDT_vect) {
-  if(f_wdt == 0) {
-    f_wdt=1;
+  if (!wdt_was_triggered) {
+    wdt_was_triggered = true;
   } else {
-    Serial.println("WDT Overrun!!!");
+    debug("WDT Overrun!!!");
   }
 }
 
 
-/***************************************************
- *  Name:        enterSleep
- *
- *  Returns:     Nothing.
- *
- *  Parameters:  None.
- *
- *  Description: Enters the arduino into sleep mode.
- *
- ***************************************************/
-void enterSleep(void)
-{
-  set_sleep_mode(SLEEP_MODE_PWR_SAVE);   /* EDIT: could also use SLEEP_MODE_PWR_DOWN for lowest power consumption. */
+void enterSleep(void) {
+  set_sleep_mode(SLEEP_MODE_PWR_SAVE); // Could also use SLEEP_MODE_PWR_DOWN for lowest power consumption
   sleep_enable();
+  sleep_mode(); // Now enter sleep mode
 
-  /* Now enter sleep mode. */
-  sleep_mode();
+  /* The program will continue from here after the WDT timeout */
 
-  /* The program will continue from here after the WDT timeout*/
-  sleep_disable(); /* First thing to do is disable sleep. */
-
-  /* Re-enable the peripherals. */
-  power_all_enable();
+  sleep_disable(); // First thing to do is disable sleep
+  power_all_enable(); // Re-enable the peripherals
 }
 
 void setupWDT() {
-    MCUSR &= ~(1<<WDRF); // Clear the reset flag.
-    WDTCSR |= (1<<WDCE) | (1<<WDE); // In order to change WDE or the prescaler, we need to set WDCE (This will allow updates for 4 clock cycles).
-    WDTCSR = 1<<WDP0 | 1<<WDP3; // Set new watchdog timeout prescaler value (8.0 seconds).
-    WDTCSR |= _BV(WDIE); // Enable the WD interrupt (note no reset).
+    MCUSR &= ~(1<<WDRF); // Clear the reset flag
+    WDTCSR |= (1<<WDCE) | (1<<WDE); // In order to change WDE or the prescaler, set WDCE (this will allow updates for 4 clock cycles)
+    WDTCSR = 1<<WDP0 | 1<<WDP3; // Set new watchdog timeout prescaler value (8.0 seconds)
+    WDTCSR |= _BV(WDIE); // Enable the WD interrupt (note no reset)
 }
 
 void setup() {
 
   Serial.begin(9600);
-  Serial.println("Initialising...");
+  debug("Initialising...");
 
   pinMode(BUTTON0, INPUT);
   pinMode(BUTTON1, INPUT);
@@ -168,47 +120,30 @@ void setup() {
 
   servo.attach(SERVO);
 
-  delay(100); //Allow for serial print to complete.
-
   attachInterrupt(digitalPinToInterrupt(BUTTON0), wakeUpButton0, RISING);
   attachInterrupt(digitalPinToInterrupt(BUTTON1), wakeUpButton1, RISING);
 
   setupWDT();
 
-  Serial.println("Initialisation complete.");
-  delay(100); //Allow for serial print to complete.
+  debug("Initialisation complete.");
 
 }
 
 
-
-/***************************************************
- *  Name:        enterSleep
- *
- *  Returns:     Nothing.
- *
- *  Parameters:  None.
- *
- *  Description: Main application loop.
- *
- ***************************************************/
-void loop()
-{
-  if(f_wdt == 1)
-  {
-    /* Toggle the LED */
-    digitalWrite(BUILTIN_LED, !digitalRead(BUILTIN_LED));
-    Serial.println("Timer interrupt");
-    delay(100); //Allow for serial print to complete.
-
-    /* Don't forget to clear the flag. */
-    f_wdt = 0;
-
-    /* Re-enter sleep mode. */
-    enterSleep();
-  }
-  else
-  {
+void loop() {
+  if (wdt_was_triggered) {
+    debug("Timer interrupt");
+    wdt_was_triggered = false;
+    enterSleep(); // Re-enter sleep mode
+  } else if (button0_was_triggered) {
+    debug("Button 0 interrupt");
+    button0_was_triggered = false;
+    enterSleep(); // Re-enter sleep mode
+  } else if (button1_was_triggered) {
+    debug("Button 1 interrupt");
+    button0_was_triggered = false;
+    enterSleep(); // Re-enter sleep mode
+  } else {
     /* Do nothing. */
   }
 }
