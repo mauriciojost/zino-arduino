733a34291013a2735512ce636bc5a36368b0ae2e
Mauricio Jost <mauricio.jost@gmail.com>
2016-10-01 00:19:13 +0200

    Improve includes script
    Also add missing libraries that are normally resolved by platformio,
    like the ones with the ping and special register mappings.

--- xtras/generate-list-of-includes.bash
+++ xtras/generate-list-of-includes.bash
@@ -2,14 +2,30 @@
 
 echo "This script aims to help adding the includes in the setup of Intellij."
 
-CURRDIR=`dirname $0`
+CURRDIR=$(readlink -e `dirname $0`)
+
+
+echo "Cleaning environment..."
+
+rm -fr lib *.list
+
+
+echo "Bringing UC libraries to an isolated library and including in the list..."
+
+mkdir lib
+cp $HOME/.platformio/packages/toolchain-atmelavr/avr/include/avr/iom328p.h lib
+echo '<include-dir path="'$CURRDIR/lib'"/>' >> includes.list
+
+
+echo "Including platformio detected libraries in the list..."
 
 cat $CURRDIR/../.gcc-flags.json | grep gccIncludePaths | sed "s/gccIncludePaths//" | sed 's/"//g' | sed 's/://g' | sed 's/,/\n/g' > libraries.list
-
-echo "" > includes.list
 while read -r line
 do
   echo '<include-dir path="'$line'"/>' >> includes.list
 done < libraries.list
+rm libraries.list
 
-echo "Replace the lines in libraries.list in the .idea/misc.xml IDEA configuration file."
+echo "Done."
+
+echo "Replace the lines in includes.list in the .idea/misc.xml IDEA configuration file."
