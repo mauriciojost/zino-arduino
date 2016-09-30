b9fbaf1194d6a7449864841755b6e1e0f0456074
Mauricio Jost <mauricio.jost@gmail.com>
2016-10-01 00:19:48 +0200

    Refactoring

--- src/Debug.h
+++ src/Debug.h
@@ -1,11 +1,14 @@
 #ifndef UNIT_TEST
+
+#define DELAY_DEBUG_MS 100
 size_t debug(const char *msg) {
   Serial.println(msg);
-  delay(100);
+  delay(DELAY_DEBUG_MS);
 }
 
 size_t debug(int msg) {
   Serial.println(msg);
-  delay(100);
+  delay(DELAY_DEBUG_MS);
 }
+
 #endif
