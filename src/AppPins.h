53ebc937e86a64d06d90d3dcf8ec742d0b1364a1
Mauricio Jost <mauricio.jost@gmail.com>
2016-10-01 20:01:25 +0200

    Sort LCD pins specification

--- src/AppPins.h
+++ src/AppPins.h
@@ -1,22 +1,32 @@
 #ifndef APP_PINS
 #define APP_PINS
 
+// ONBOARD
+#define BUILTIN_LED UC_PIN_13
+
+// BUTTONS
 #define BUTTON0 UC_PIN_2
 #define BUTTON1 UC_PIN_3
 
-#define BUILTIN_LED UC_PIN_13
-
+// SERVO
 #define SERVO_PIN UC_PIN_A2
 
+// LCD
+#define LCD_VSS GROUND
+#define LCD_VCC VCC
+#define LCD_V0 GROUND
 #define LCD_RS_PIN UC_PIN_12
+#define LCD_RW GROUND
 #define LCD_ENABLE_PIN UC_PIN_11
+#define LCD_D0_PIN AIR
+#define LCD_D1_PIN AIR
+#define LCD_D2_PIN AIR
+#define LCD_D3_PIN AIR
 #define LCD_D4_PIN UC_PIN_4
 #define LCD_D5_PIN UC_PIN_5
 #define LCD_D6_PIN UC_PIN_6
 #define LCD_D7_PIN UC_PIN_7
-#define LCD_RW GROUND
-#define LCD_VSS GROUND
-#define LCD_VCC VCC
-#define LCD_V0 GROUND
+#define LCD_A RES5KVCC
+#define LCD_K GROUND
 
 #endif
