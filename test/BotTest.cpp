b5b03cacd9b3f5eefde83a07d0e255145681d680
Mauricio Jost <mauricio.jost@gmail.com>
2016-10-01 02:29:54 +0200

    First meaningful test

--- test/BotTest.cpp
+++ test/BotTest.cpp
@@ -1,23 +1,30 @@
 #include <unity.h>
 #include <Bot.h>
 
+#define BUTTON0_PRESSED true
+#define BUTTON1_PRESSED true
+#define WDT_INTERRUPT true
+
 #ifdef UNIT_TEST
 
-void setUp(void) {
+void setUp(void) {}
 
-}
+void tearDown(void) {}
 
-void tearDown(void) {
-
-}
-
-void test_led_builtin_pin_number(void) {
-  TEST_ASSERT_EQUAL(13, 13);
+void test_bot_correctly_switches_states(void) {
+  Bot bot;
+  TEST_ASSERT_EQUAL(bot.state, Display);
+  bot.run(false, false, false);
+  TEST_ASSERT_EQUAL(bot.state, Display);
+  bot.run(BUTTON0_PRESSED, false, false);
+  TEST_ASSERT_EQUAL(bot.state, Run);
+  bot.run(false, BUTTON1_PRESSED, false);
+  TEST_ASSERT_EQUAL(bot.state, Display);
 }
 
 int main() {
   UNITY_BEGIN(); // IMPORTANT LINE!
-  RUN_TEST(test_led_builtin_pin_number);
+  RUN_TEST(test_bot_correctly_switches_states);
   UNITY_END(); // stop unit testing
 }
 
