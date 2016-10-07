50561d6077ec7bc04f3bbf27493c0d8012616bfa
Mauricio Jost <mauricio.jost@gmail.com>
2016-10-07 21:01:56 +0200

    Add Clock.isTimeToWater with different frequencies

--- src/Clock.h
+++ src/Clock.h
@@ -7,6 +7,12 @@
 #define SECONDS_IN_HOUR 3600
 #define SECONDS_IN_MINUTE 60
 
+#define ONCE_H 24
+#define ONCE_M 60
+#define ONCE_S 60
+
+#define ANY 10000
+
 enum Frequency {
   OncePerMonth = 0,
   TwicePerMonth = 1,
@@ -14,7 +20,7 @@ enum Frequency {
   TwicePerWeek = 3,
   ThreeTimesPerWeek = 4,
   OncePerDay = 5,
-  TwicePerDayPerDay = 6,
+  TwicePerDay = 6,
 };
 
 class Clock {
@@ -23,16 +29,40 @@ public:
   uint8_t secondsOffset;
   uint32_t cyclesFromMidnight;
 
+  bool isTimeToWater(Frequency f) {
+    bool ret = false;
+    switch (f) {
+      case OncePerMonth:      ret = isTime(30, ONCE_H, ONCE_M, ONCE_S); break;
+      case TwicePerMonth:     ret = isTime(15, ONCE_H, ONCE_M, ONCE_S); break;
+      case OncePerWeek:       ret = isTime(07, ONCE_H, ONCE_M, ONCE_S); break;
+      case TwicePerWeek:      ret = isTime(03, ONCE_H, ONCE_M, ONCE_S); break;
+      case ThreeTimesPerWeek: ret = isTime(02, ONCE_H, ONCE_M, ONCE_S); break;
+      case OncePerDay:        ret = isTime(ANY,ONCE_H, ONCE_M, ONCE_S); break;
+      case TwicePerDay:       ret = isTime(ANY,    12, ONCE_M, ONCE_S); break;
+      default: ret = false; break;
+    }
+    return ret;
+  }
+
+  bool isTime(uint32_t day, uint32_t hour, uint32_t minute, uint32_t second) {
+    bool matchesDays =  ((this->getDays() % day) == 0)       || (day == ANY);
+    bool matchesHours = ((this->getHours() % hour) == 0)     || (hour == ANY);
+    bool matchesMinutes = ((this->getMinutes() % minute) == 0) || (minute == ANY);
+    bool matchesSeconds = ((this->getSeconds() % second) == 0) || (second == ANY);
+    bool allMatch = matchesDays && matchesHours && matchesMinutes && matchesSeconds;
+    return allMatch;
+  }
+
   Clock() {
-    this->set(0, 0, 0);
+    this->set(0, 0, 0, 0);
   }
 
   void tick() {
     this->cyclesFromMidnight++;
   }
 
-  void set(uint32_t hours, uint32_t minutes, uint32_t seconds) {
-    uint32_t secondsFromMidnight = hours * SECONDS_IN_HOUR + minutes * SECONDS_IN_MINUTE + seconds;
+  void set(uint32_t days, uint32_t hours, uint32_t minutes, uint32_t seconds) {
+    uint32_t secondsFromMidnight = days * SECONDS_IN_DAY + hours * SECONDS_IN_HOUR + minutes * SECONDS_IN_MINUTE + seconds;
     this->cyclesFromMidnight = secondsFromMidnight / INTERNAL_CYCLE_TO_SECONDS_FACTOR;
     this->secondsOffset = secondsFromMidnight - this->cyclesFromMidnight * INTERNAL_CYCLE_TO_SECONDS_FACTOR;
   }
--- test/ClockTest.cpp
+++ test/ClockTest.cpp
@@ -15,22 +15,52 @@ void tearDown(void) {}
 
 void test_clock_correctly_ticks(void) {
   Clock clock;
-  for (int h=0; h<24; h++) {
-    for (int m=0; m<60; m++) {
-      for (int s=0; s<60; s++) {
-        clock.set(h, m, s);
-        TEST_ASSERT_EQUAL(h, clock.getHours());
-        TEST_ASSERT_EQUAL(m, clock.getMinutes());
-        TEST_ASSERT_EQUAL(s, clock.getSeconds());
+  for (int d=0; d<31; d++) {
+    for (int h=0; h<24; h++) {
+      for (int m=0; m<60; m++) {
+        for (int s=0; s<60; s++) {
+          clock.set(d, h, m, s);
+          TEST_ASSERT_EQUAL(d, clock.getDays());
+          TEST_ASSERT_EQUAL(h, clock.getHours());
+          TEST_ASSERT_EQUAL(m, clock.getMinutes());
+          TEST_ASSERT_EQUAL(s, clock.getSeconds());
+        }
       }
     }
   }
-
 }
+
+int count_waterings_in_30days(Frequency f) {
+  int count = 0;
+  Clock clock;
+  for (int d=0; d<30; d++) {
+    for (int h=0; h<24; h++) {
+      for (int m=0; m<60; m++) {
+        for (int s=0; s<60; s++) {
+          clock.set(d, h, m, s);
+          if (clock.isTimeToWater(f)) count++;
+        }
+      }
+    }
+  }
+  return count;
+}
+
+void test_clock_correctly_tells_time_to_water(void) {
+  TEST_ASSERT_EQUAL(1, count_waterings_in_30days(OncePerMonth));
+  TEST_ASSERT_EQUAL(2, count_waterings_in_30days(TwicePerMonth));
+  TEST_ASSERT_EQUAL(5, count_waterings_in_30days(OncePerWeek));
+  TEST_ASSERT_EQUAL(10, count_waterings_in_30days(TwicePerWeek));
+  TEST_ASSERT_EQUAL(15, count_waterings_in_30days(ThreeTimesPerWeek));
+  TEST_ASSERT_EQUAL(30, count_waterings_in_30days(OncePerDay));
+  TEST_ASSERT_EQUAL(60, count_waterings_in_30days(TwicePerDay));
+}
+
 
 int main() {
   UNITY_BEGIN();
   RUN_TEST(test_clock_correctly_ticks);
+  RUN_TEST(test_clock_correctly_tells_time_to_water);
   UNITY_END();
 }
 
