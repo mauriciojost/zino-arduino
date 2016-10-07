052be81099c69e263ffbab3c81f977133078826a
Mauricio Jost <mauricio.jost@gmail.com>
2016-10-07 19:28:50 +0200

    Add enums for frequency

--- src/Clock.h
+++ src/Clock.h
@@ -2,8 +2,20 @@
 #include <Misc.h>
 
 #define INTERNAL_CYCLE_TO_SECONDS_FACTOR 8.0f
+
+#define SECONDS_IN_DAY (SECONDS_IN_HOUR * 24)
 #define SECONDS_IN_HOUR 3600
 #define SECONDS_IN_MINUTE 60
+
+enum Frequency {
+  OncePerMonth = 0,
+  TwicePerMonth = 1,
+  OncePerWeek = 2,
+  TwicePerWeek = 3,
+  ThreeTimesPerWeek = 4,
+  OncePerDay = 5,
+  TwicePerDayPerDay = 6,
+};
 
 class Clock {
 
@@ -25,12 +37,16 @@ public:
     this->secondsOffset = secondsFromMidnight - this->cyclesFromMidnight * INTERNAL_CYCLE_TO_SECONDS_FACTOR;
   }
 
+  uint32_t getDays() {
+    return this->getSecondsFromMidnight() / SECONDS_IN_DAY;
+  }
+
   uint32_t getHours() {
-    return this->getSecondsFromMidnight() / SECONDS_IN_HOUR;
+    return (this->getSecondsFromMidnight() % SECONDS_IN_DAY) / SECONDS_IN_HOUR;
   }
 
   uint32_t getMinutes() {
-    return ((this->getSecondsFromMidnight() % SECONDS_IN_HOUR)- this->getSeconds()) / SECONDS_IN_MINUTE ;
+    return (this->getSecondsFromMidnight() % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE ;
   }
 
   uint32_t getSeconds() {
