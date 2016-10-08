33daf535846bd95a6946f2c494ad3544bbeac730
Mauricio Jost <mauricio.jost@gmail.com>
2016-10-08 02:11:19 +0200

    Remove time related functions from Misc

--- src/Misc.h
+++ src/Misc.h
@@ -9,20 +9,6 @@
 #define rollValue(amt, low, high)                                         \
   ((amt) < (low) ? (low) : ((amt) > (high) ? (low) : (amt)))
 
-void toHourMinuteSecondsString(uint32_t seconds, char* msg) {
-  int sec = seconds % 60;
-  int min = (seconds / 60) % 60;
-  int hou = (seconds / 3600);
-  sprintf(msg, "%02d:%02d:%02d", hou, min, sec);
-}
-
-void toDayHourMinutesString(uint32_t seconds, char* msg) {
-  int min = (seconds / 60) % 60;
-  int hou = (seconds / 3600) % 24;
-  int day = (seconds / (3600 * 24));
-  sprintf(msg, "%d days %02d:%02d", day, hou, min);
-}
-
 float toRadians(int degrees) {
   return (((float)degrees) / 360) * 2 * M_PI;
 }
--- test/MiscTest.cpp
+++ test/MiscTest.cpp
@@ -12,44 +12,6 @@
 #define ANGLE_FOR_FRACTION_010 47
 #define ANGLE_FOR_FRACTION_005 37
 
-
-void test_to_hour_minute_seconds_string(void) {
-  char buffer[16];
-
-  int second = 1;
-  int minute = 60 * second;
-  int hour = 60 * minute;
-
-  toHourMinuteSecondsString(hour, buffer);
-  TEST_ASSERT_EQUAL_STRING("01:00:00", buffer);
-
-  toHourMinuteSecondsString(minute, buffer);
-  TEST_ASSERT_EQUAL_STRING("00:01:00", buffer);
-
-  toHourMinuteSecondsString(second, buffer);
-  TEST_ASSERT_EQUAL_STRING("00:00:01", buffer);
-
-  toHourMinuteSecondsString(hour + minute + second, buffer);
-  TEST_ASSERT_EQUAL_STRING("01:01:01", buffer);
-
-  toHourMinuteSecondsString(2 * hour + 5 * minute + 10 * second, buffer);
-  TEST_ASSERT_EQUAL_STRING("02:05:10", buffer);
-
-}
-
-void test_to_day_hour_minutes_string(void) {
-  char buffer[16];
-
-  int second = 1;
-  int minute = 60 * second;
-  int hour = 60 * minute;
-  int day = 24 * hour;
-
-  toDayHourMinutesString(hour, buffer);
-  TEST_ASSERT_EQUAL_STRING("0 days 01:00", buffer);
-
-}
-
 void test_water_amounts_mapping(void) {
 
   TEST_ASSERT_EQUAL(0.0f, fractionPooredWater(0));
@@ -78,8 +40,6 @@ void test_servo_position(void) {
 
 int main() {
   UNITY_BEGIN();
-  RUN_TEST(test_to_hour_minute_seconds_string);
-  RUN_TEST(test_to_day_hour_minutes_string);
   RUN_TEST(test_water_amounts_mapping);
   RUN_TEST(test_servo_position);
   UNITY_END();
