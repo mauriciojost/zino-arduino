25c5c5f26b8ad1750161ad2feca007cba59f8385
Mauricio Jost <mauricio.jost@gmail.com>
2016-10-13 23:15:13 +0200

    Add Pump

---
+++ src/Pump.cpp
@@ -0,0 +1,31 @@
+#include <Pump.h>
+
+Pump::Pump(){
+  this->waterAmountPerShot = DEFAULT_WATER_PUMP_AMOUNT_PER_SHOT;
+  this->state = PumpOffState;
+}
+
+void Pump::cycle(bool mustWaterNow) {
+  if (mustWaterNow) {
+    log(Debug, "  PMP: ->ON");
+    this->state = PumpOnState;
+    this->cyclesOfWateringLeft = this->waterAmountPerShot;
+  } else if (this->cyclesOfWateringLeft != 0) {
+    log(Debug, "  PMP: ->ON (STILL)");
+    this->state = PumpOnState;
+    this->cyclesOfWateringLeft = constrainValue(this->cyclesOfWateringLeft - 1, 0, MAX_WATER_PUMP_AMOUNT_PER_SHOT);
+  } else {
+    log(Debug, "  PMP: ->OFF");
+    this->state = PumpOffState;
+  }
+}
+
+bool Pump::isPumpDriven() {
+  return (this->state == PumpOnState);
+}
+
+void Pump::nextWaterAmountPerShot() {
+  this->waterAmountPerShot = rollValue(this->waterAmountPerShot + INCR_WATER_PUMP_AMOUNT_PER_SHOT,
+            MIN_WATER_PUMP_AMOUNT_PER_SHOT, MAX_WATER_PUMP_AMOUNT_PER_SHOT);
+}
+
---
+++ src/Pump.h
@@ -0,0 +1,35 @@
+#ifndef PUMP_INC
+#define PUMP_INC
+
+#include <Log.h>
+#include <Misc.h>
+
+#define MIN_WATER_PUMP_AMOUNT_PER_SHOT 1
+#define DEFAULT_WATER_PUMP_AMOUNT_PER_SHOT 10
+#define MAX_WATER_PUMP_AMOUNT_PER_SHOT 60
+#define INCR_WATER_PUMP_AMOUNT_PER_SHOT 1
+
+enum PumpState {
+  PumpOnState = 0,
+  PumpOffState = 1,
+  DelimiterAmountOfPumpStates = 2
+};
+
+class Pump {
+
+private:
+
+public:
+  PumpState state;
+  unsigned int waterAmountPerShot; // expressed in amount of cycles where the pump is on
+  unsigned int cyclesOfWateringLeft;
+
+  Pump();
+  void cycle(bool mustWaterNow);
+  bool isPumpDriven();
+  void nextWaterAmountPerShot();
+
+};
+
+#endif // PUMP_INC
+
