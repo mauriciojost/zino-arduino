428c95f19e099771edac69e4365e9ce51c823d55
Mauricio Jost <mauricio.jost@gmail.com>
2016-10-29 11:53:25 +0200

    Make INTERNAL_CYCLE_TO_SECONDS available outside Clock too

--- src/Misc.h
+++ src/Misc.h
@@ -6,4 +6,10 @@
 #define constrainValue(amt, low, high) ((amt) < (low) ? (low) : ((amt) > (high) ? (high) : (amt)))
 #define rollValue(amt, low, high) ((amt) < (low) ? (high) : ((amt) > (high) ? (low) : (amt)))
 
+#ifdef CYCLE_OF_1S
+#define INTERNAL_CYCLE_TO_SECONDS_FACTOR 1.024f
+#else // CYCLE_OF_8S
+#define INTERNAL_CYCLE_TO_SECONDS_FACTOR 8.192f
+#endif
+
 #endif // MISC_INC
