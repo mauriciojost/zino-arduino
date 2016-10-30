7f3cdeb427fb2ceb06bf31903369f3c1df9e36e6
Mauricio Jost <mauricio.jost@gmail.com>
2016-10-30 11:59:16 +0100

    Fix TestActor LCD messages

--- src/TestActor.cpp
+++ src/TestActor.cpp
@@ -22,16 +22,12 @@ int TestActor::getActorState() { return on; }
 void TestActor::setConfig(int configIndex, char *retroMsg, bool set) {
   switch (configIndex) {
   case (TestActorConfigStateAmount):
-    if (set) {
-      log(Debug, "  ACT CNF: 1");
-      sprintf(retroMsg, "CNF 1 %s", name);
-    }
+    log(Debug, "  ACT CNF: 1");
+    sprintf(retroMsg, "CNF 1");
     break;
   case (TestActorConfigStateAmount2):
-    if (set) {
-      log(Debug, "  ACT CNF: 1");
-      sprintf(retroMsg, "CNF 2 %s", name);
-    }
+    log(Debug, "  ACT CNF: 1");
+    sprintf(retroMsg, "CNF 2");
     break;
   default:
     break;
@@ -43,10 +39,10 @@ int TestActor::getNroConfigs() { return (int)TestActorConfigStateDelimiter; }
 void TestActor::getInfo(int infoIndex, char *retroMsg) {
   switch (infoIndex) {
   case (TestActorInfoConfig):
-    sprintf(retroMsg, "INFO1 %s", name);
+    sprintf(retroMsg, "INFO1");
     break;
   case (TestActorLastWatered):
-    sprintf(retroMsg, "INFO2 %s", name);
+    sprintf(retroMsg, "INFO2");
     break;
   default:
     break;
