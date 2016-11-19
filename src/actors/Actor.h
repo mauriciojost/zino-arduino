d138ddc2f3122bde9cde5d564a37ddf7f5d22512
Mauricio Jost <mauricio.jost@gmail.com>
2016-11-19 18:34:46 +0100

    Remove unneeded method declarations from Actor

--- src/actors/Actor.h
+++ src/actors/Actor.h
@@ -13,11 +13,7 @@
 class Actor : public Configurable {
 
 public:
-  /**
-  * Retrieve the name of the actor.
-  */
-  virtual const char *getName() = 0;
-
+  
   /**
   * Make the actor execute a cycle.
   */
@@ -28,25 +24,6 @@ public:
   */
   virtual int getActuatorValue() = 0;
 
-  /**
-  * Get the number of configuration states of the actor.
-  */
-  virtual int getNroConfigs() = 0;
-
-  /**
-  * Retrieve configuration message given its index, and set the configuration if requested.
-  */
-  virtual void setConfig(int configIndex, char *retroMsg, bool set) = 0;
-
-  /**
-  * Get the number of info states of the actor.
-  */
-  virtual int getNroInfos() = 0;
-
-  /**
-  * Get an information status of the actor.
-  */
-  virtual void getInfo(int infoIndex, char *retroMsg) = 0;
 };
 
 #endif // ACTOR_INC
