d095e83ece9e7cc91fe03e62353778f3f5ac4836
Mauricio Jost <mauricio.jost@gmail.com>
2016-11-22 20:16:54 +0100

    Move colours from Misc to Log

--- src/Log.h
+++ src/Log.h
@@ -6,6 +6,15 @@
 #define DELAY_DEBUG_MS 5
 #define SERIAL_BAUDS 115200
 
+// ON-PC SHELL COLORS (to be used with printf in PC tests)
+// http://ascii-table.com/ansi-escape-sequences.php
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KMAG "\x1B[35m"
+#define KYEL "\x1B[33m"
+#define KBLU "\x1B[34m"
+#define KWHTBLU "\x1B[37;44m"
+
 enum LogLevel { Debug = 0, Info = 1, Warn = 2, Error = 3 };
 
 void setupLog();
--- src/Misc.h
+++ src/Misc.h
@@ -9,15 +9,6 @@
 #define constrainValue(amt, low, high) ((amt) < (low) ? (low) : ((amt) > (high) ? (high) : (amt)))
 #define rollValue(amt, low, high) ((amt) < (low) ? (high) : ((amt) > (high) ? (low) : (amt)))
 
-// Colors to be used with printf in PC tests
-// http://ascii-table.com/ansi-escape-sequences.php
-#define KNRM "\x1B[0m"
-#define KRED "\x1B[31m"
-#define KMAG "\x1B[35m"
-#define KYEL "\x1B[33m"
-#define KBLU "\x1B[34m"
-#define KWHTBLU "\x1B[37;44m"
-
 #define LCD_LENGTH 16
 
 #endif // MISC_INC
