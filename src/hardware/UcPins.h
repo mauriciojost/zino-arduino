005213cf04a2c39f8096bf676a93bcd9133638b7
Mauricio Jost <mauricio.jost@gmail.com>
2016-11-22 21:52:10 +0100

    Define pins even when running on PC

--- src/hardware/UcPins.h
+++ src/hardware/UcPins.h
@@ -15,6 +15,8 @@
 #define UC_PIN_12 12
 #define UC_PIN_13 13
 
+#ifndef UNIT_TEST // On arduino
+
 #define UC_PIN_A0 A0
 #define UC_PIN_A1 A1
 #define UC_PIN_A2 A2
@@ -23,5 +25,21 @@
 #define UC_PIN_A5 A5
 #define UC_PIN_A6 A6
 #define UC_PIN_A7 A7
+
+#else // UNIT_TEST, on PC
+
+#define UC_PIN_A0 0
+#define UC_PIN_A1 1
+#define UC_PIN_A2 2
+#define UC_PIN_A3 3
+#define UC_PIN_A4 4
+#define UC_PIN_A5 5
+#define UC_PIN_A6 6
+#define UC_PIN_A7 7
+
+#define LOW 0
+#define HIGH 1
+
+#endif // UNIT_TEST
 
 #endif // UC_PINS_INC
