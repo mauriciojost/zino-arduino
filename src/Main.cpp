0e3f046ec7e3b57ebdb5a8e5e58a472b5731d432
Mauricio Jost <mauricio.jost@gmail.com>
2016-11-23 19:19:08 +0100

    Put project under GNU GPL v3

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,25 @@
+/*
+ * ZINO: low-budget autonomous plant care system.
+ *
+ * Copyright 2016 Mauricio Jost
+ * Contact: mauriciojostx@gmail.com
+ *
+ * This file is part of ZINO.
+ *
+ * ZINO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * ZINO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ZINO. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef UNIT_TEST
 
 #include <Main.h>
