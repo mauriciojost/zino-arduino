6ed187806b055f1bd802c9457e05d8524010877e
Mauricio Jost <mauricio.jost@gmail.com>
2016-11-23 19:18:00 +0100

    Remove MiscTest

--- misc/test.bash
+++ misc/test.bash
@@ -11,7 +11,6 @@ platformio run
 platformio test -e module-test
 platformio test -e delayer-test
 platformio test -e bot-test
-platformio test -e misc-test
 platformio test -e clock-test
 platformio test -e pump-test
 platformio test -e level-test
