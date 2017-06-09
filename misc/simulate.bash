9bd4aaaa6fddad75c0a0a8e76d71138600665eda
Mauricio Jost <mauricio.jost@gmail.com>
2017-06-09 20:58:20 +0200

    Refactor simulate.bash

--- misc/simulate.bash
+++ misc/simulate.bash
@@ -3,12 +3,15 @@
 set -x 
 set -e 
 
+CURR_DIR=$(readlink -e `dirname $0`)
+ROOT_DIR="$CURR_DIR/../"
+
 LOGS=${1:-0}
 SIMULATOR_BIN=simulator.bin
 LIBS="-lpthread"
 FLAGS="-D SIMULATION_MODULE -D UNIT_TEST -D SUBCYCLES_2 -D LOG_LEVEL=$LOGS -D BINARY_LEVEL"
-SRC="src/*.cpp src/actors/*.cpp src/ui/*.cpp src/hardware/*.cpp"
-HEADERS="-I src/ -I src/ui/ -I src/actors/ -I src/hardware/"
+SRC="$ROOT_DIR/src/*.cpp $ROOT_DIR/src/actors/*.cpp $ROOT_DIR/src/ui/*.cpp $ROOT_DIR/src/hardware/*.cpp"
+HEADERS="-I $ROOT_DIR/src/ -I $ROOT_DIR/src/ui/ -I $ROOT_DIR/src/actors/ -I $ROOT_DIR/src/hardware/"
 
 g++ -o $SIMULATOR_BIN $FLAGS test/Simulation.cpp $SRC $HEADERS $LIBS
 ./$SIMULATOR_BIN
